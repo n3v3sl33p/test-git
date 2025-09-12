088bc987f83cac11c48813e7893bac6bf1afedf2
Artem <yatsunenkoaa@fsk.ru>
2025-09-12 16:10:45 +0300

    feature realise

---
+++ file1.py
@@ -0,0 +1,3 @@
+def hello_world():
+    """Prints 'Hello world!'."""
+    print("Hello world!")
---
+++ file2.py
@@ -0,0 +1,8 @@
+def add_numbers(a, b):
+    """Adds two numbers"""
+    return a + b
+
+
+def multiply_numbers(a, b):
+    """Multiplies two numbers"""
+    return a * b
